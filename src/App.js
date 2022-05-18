e7020610e83a77a8843bc3818c60c36e37f7e410
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-19 02:43:23 +0600

    notification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 
 
 function App() {
   return (
     <div className="App">
+      <Toaster toastOptions={{ duration: 3000 }}></Toaster>
       <Home></Home>
     </div>
   );
--- src/pages/SingleTask.jsx
+++ src/pages/SingleTask.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const SingleTask = ({ task, refetch }) => {
-  const [completed, setCompleted] = useState(false)
+  const [pending, setPompleted] = useState(false)
   const { name, description, _id } = task
   const handleDelete = () => {
     axios.delete(`http://localhost:5000/task/${_id}`).then((res) => {
@@ -10,15 +11,20 @@ const SingleTask = ({ task, refetch }) => {
       refetch()
     })
   }
+
+  const handleComplete = () => {
+    setPompleted(!pending)
+    if (!pending) toast.success('task compelted')
+  }
   return (
     <div className='card-wide grid grid-cols-2 items-center'>
       <div>
-        <h2 className={`${completed && 'line-through'} title-start`}>{name}</h2>
-        <p className={`${completed && 'line-through'}`}>{description}</p>
+        <h2 className={`${pending && 'line-through'} title-start`}>{name}</h2>
+        <p className={`${pending && 'line-through'}`}>{description}</p>
       </div>
       <div className='justify-self-center space-x-1'>
         <button
-          onClick={() => setCompleted(!completed)}
+          onClick={handleComplete}
           className='btn-accent justify-self-end'
         >
           complete
